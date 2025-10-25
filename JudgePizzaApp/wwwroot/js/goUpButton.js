25014a58ebc9214d5c8d228fcc9634f555ae3dc1
Taha PARLAR <tahaparlarr@gmail.com>
2025-10-25 13:31:00 +0300

    add footer and go up btn

---
+++ JudgePizzaApp/wwwroot/js/goUpButton.js
@@ -0,0 +1,16 @@
+﻿const goUpBtn = document.getElementById("goUpBtn");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY < 200) {
+        goUpBtn.classList.add("d-none");
+    } else {
+        goUpBtn.classList.remove("d-none");
+    }
+});
+
+goUpBtn.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
